6c8895d3580a464c3b7cac1e82e3c079dcb31e4a
Tim Freund <tim@freunds.net>
2015-07-17 15:00:31 -0400

    Extract data from getfile.cfm

--- fetchvalues.py
+++ fetchvalues.py
@@ -7,7 +7,21 @@ from urllib.request import urlopen
 METADATA_URL = 'http://www.lcswma.org/dashboard/js/functions.js'
 DATA_URL = 'http://www.lcswma.org/dashboard/getfile.cfm'
 
+def fetch_data(metadata):
+    data = urlopen(DATA_URL)
+    data = str(data.read())
+
+    values = data.split("<br>")
+    date = values[0]
+    time = values[1]
+    print("at %s on %s" % (time, date))
+
+    for idx in metadata.keys():
+        print("%s :: %s" % (metadata[idx], values[idx]))
+
 def fetch_metadata():
+    metadata = {}
+
     key_re = re.compile("(?<=span#).*(?='\).html)")
     idx_re = re.compile("[0-9]+")
 
@@ -16,8 +30,10 @@ def fetch_metadata():
         line = str(line)
         if line.count('.html(theValues'):
             key = key_re.search(line).group(0).replace("\\", "")
-            idx = idx_re.search(line).group(0)
-            print("%s :: %s" % (key, idx))
+            idx = int(idx_re.search(line).group(0))
+            metadata[idx] = key
+    return metadata
 
 if __name__ == '__main__':
-    fetch_metadata()
+    metadata = fetch_metadata()
+    fetch_data(metadata)
